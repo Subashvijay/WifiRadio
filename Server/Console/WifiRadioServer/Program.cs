4624cd172b8cd923753314501bdcd47c9c0729bf
subash bose <subash.bose@siemens.com>
2024-06-27 19:58:11 +0530

    .

--- Server/Console/WifiRadioServer/Program.cs
+++ Server/Console/WifiRadioServer/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
 using NAudio.Wave;
 
 Console.WriteLine("Hello, World!");
@@ -8,8 +9,11 @@ using (var outputDevice = new WaveOutEvent())
 {
     outputDevice.Init(audioFile);
     outputDevice.Play();
+    Stopwatch st = new();
     while (outputDevice.PlaybackState == PlaybackState.Playing)
     {
+        Console.Clear();
+        Console.WriteLine(st.ElapsedMilliseconds / 1000);
         Thread.Sleep(1000);
     }
 }
