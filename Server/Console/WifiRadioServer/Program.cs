65d72bf3abf8d8e07363be7786e9eb53313025a6
subash bose <subash.bose@siemens.com>
2024-06-27 15:38:18 +0530

    init

---
+++ Server/Console/WifiRadioServer/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NAudio.Wave;
+
+Console.WriteLine("Hello, World!");
+var audioFileS = @"D:\Music\Aadi-Pona-Aavani.mp3";
+using (var audioFile = new AudioFileReader(audioFileS))
+using (var outputDevice = new WaveOutEvent())
+{
+    outputDevice.Init(audioFile);
+    outputDevice.Play();
+    while (outputDevice.PlaybackState == PlaybackState.Playing)
+    {
+        Thread.Sleep(1000);
+    }
+}
